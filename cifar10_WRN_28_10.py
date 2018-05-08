d701fedef4b64b6e0021c08b415f0ab6e1cbae21
Akshay Chawla <chawla.akshay1234@gmail.com>
2018-05-08 14:11:23 +0530

    CIFAR 10 Baseline implementation + Wide Residual Network + 28 layers, feature multiplier i.e k=10 + Original Paper: test error = 4.00 (this is what I have to reach)

---
+++ cifar10_WRN_28_10.py
@@ -0,0 +1,89 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import os, sys 
+import wide_residual_network as wrn 
+from keras.datasets import cifar10 
+from keras.callbacks import Callback, ModelCheckpoint, LearningRateScheduler, TensorBoard 
+from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD 
+
+# hyperparameters 
+batch_size = 128 
+epochs   = 200 
+img_rows, img_cols = 32, 32 
+weight_decay = 0.0005 
+root_folder = "./baseline/"
+
+# Dataset 
+(trainX, trainY), (testX, testY) = cifar10.load_data()
+trainY = to_categorical(trainY)
+testY = to_categorical(testY)
+
+# Data generators 
+train_dgen = ImageDataGenerator(
+                featurewise_center=True,
+                featurewise_std_normalization=True,
+                horizontal_flip=True, 
+                width_shift_range=4, # mimick padding=4 + randomcrop 
+                height_shift_range=4, 
+                fill_mode="nearest"
+            )
+train_dgen.fit(trainX) 
+test_dgen = ImageDataGenerator(
+                featurewise_center=True, 
+                featurewise_std_normalization=True
+            )
+test_dgen.fit(trainX) # IMP! mean,std calculated on training data
+
+# Callbacks 
+def lr_scheduler_fxn(epoch):
+    if epoch<60:
+        return 0.1 
+    elif epoch<120:
+        return 0.02
+    elif epoch<160:
+        return 0.004
+    else:
+        return 0.0008
+
+lrschedule = LearningRateScheduler(lr_scheduler_fxn, verbose=1) 
+tboard     = TensorBoard(log_dir=os.path.join(root_folder, "logs"))
+checkpoint = ModelCheckpoint(
+                filepath=os.path.join(root_folder, "checkpoints", 
+                                    "wts_{epoch:02d}-{loss:.2f}.h5"),
+                verbose=True, 
+                save_weights_only=True
+            )
+
+# Network 
+model = wrn.create_wide_residual_network(
+            (32,32,3),
+            nb_classes=10, 
+            N=4, k=10, 
+            dropout=0.0
+        )
+print(model.summary())
+sgd = SGD(lr=0.1, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd) 
+
+# Train 
+model.fit_generator(
+        train_dgen.flow(trainX, trainY, batch_size=batch_size),
+        steps_per_epoch=len(trainX) // batch_size + 1, 
+        validation_data=test_dgen.flow(testX, testY, batch_size=batch_size),
+        validation_steps=len(testX) // batch_size + 1,
+        epochs=epochs,
+        callbacks=[lrschedule, tboard, checkpoint]
+    )
+
+
+
+
+
+
+
+
+
+
+
