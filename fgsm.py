a0354f2a694ef201890585e25d7602186de7b8eb
Priyam Tejaswin <priyamtejaswin@gmail.com>
2018-06-09 15:16:07 +0530

    For `performance_post_attack`, shuffle was previously True. It's now False.
     Changes to be committed:
    	modified:   fgsm.py

--- fgsm.py
+++ fgsm.py
@@ -1,51 +1,51 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 import os, sys, time, pickle
-import wide_residual_network as wrn 
-from keras.datasets import cifar10 
-from keras.callbacks import Callback, ModelCheckpoint, LearningRateScheduler, TensorBoard 
+import wide_residual_network as wrn
+from keras.datasets import cifar10
+from keras.callbacks import Callback, ModelCheckpoint, LearningRateScheduler, TensorBoard
 from keras.utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD 
-import keras.backend as K 
-from keras import losses 
+from keras.optimizers import SGD
+import keras.backend as K
+from keras import losses
 from tqdm import *
 
 def create_gradient_function(model, input_idx, output_idx):
     gt_tensor     = K.placeholder(shape=(None, 10))
-    input_tensor  = model.inputs[input_idx] 
+    input_tensor  = model.inputs[input_idx]
     output_tensor = model.outputs[output_idx]
-    loss_tensor   = losses.categorical_crossentropy(gt_tensor, output_tensor) 
+    loss_tensor   = losses.categorical_crossentropy(gt_tensor, output_tensor)
     grads_tensor  = K.gradients(loss_tensor, input_tensor)[0]
 
     get_gradients_function = K.function(
-                                inputs=[input_tensor, gt_tensor], 
+                                inputs=[input_tensor, gt_tensor],
                                 outputs=[loss_tensor, grads_tensor])
     return get_gradients_function
 
 
 if __name__ == "__main__":
 
-    # Network 
+    # Network
     model = wrn.create_wide_residual_network(
                 (32,32,3),
-                nb_classes=10, 
-                N=4, k=10, 
+                nb_classes=10,
+                N=4, k=10,
                 dropout=0.0
             )
-    
-    # load wts 
+
+    # load wts
     model.compile(loss="categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
     if len(sys.argv) == 1:
         print("WARNING: No checkpoint specified!, using random weights")
-    else: 
+    else:
         print("Loading weights from location: ", sys.argv[1])
-        wts_location = sys.argv[1] 
-        model.load_weights(wts_location)  #Load weights 
+        wts_location = sys.argv[1]
+        model.load_weights(wts_location)  #Load weights
 
-    
+
     # import ipdb; ipdb.set_trace()
-    # Dataset 
+    # Dataset
     (trainX, trainY), (testX, testY) = cifar10.load_data()
     trainY = to_categorical(trainY)
     testY = to_categorical(testY)
@@ -54,58 +54,56 @@ if __name__ == "__main__":
     train_dgen = ImageDataGenerator(
                     featurewise_center=True,
                     featurewise_std_normalization=True,
-                    horizontal_flip=True, 
-                    width_shift_range=4, # mimick padding=4 + randomcrop 
-                    height_shift_range=4, 
+                    horizontal_flip=True,
+                    width_shift_range=4, # mimick padding=4 + randomcrop
+                    height_shift_range=4,
                     fill_mode="nearest"
                 )
-    train_dgen.fit(trainX) 
+    train_dgen.fit(trainX)
     test_dgen = ImageDataGenerator(
-                    featurewise_center=True, 
+                    featurewise_center=True,
                     featurewise_std_normalization=True
                 )
     test_dgen.fit(trainX) # IMP! mean,std calculated on training data
 
-    # FGSM parameters 
-    import ipdb; ipdb.set_trace()
+    # FGSM parameters
+    # import ipdb; ipdb.set_trace()
     eta = 0.007
     if len(sys.argv) == 1:
         print("Setting default value of eta...")
     else:
         eta = float(sys.argv[2])
 
-    # Performance before attack 
+    # Performance before attack
     performance_pre_attack = model.evaluate_generator(
                             generator = test_dgen.flow(testX, testY, batch_size=50, shuffle=False),
-                            steps = len(testX)/50, 
+                            steps = len(testX)/50,
                             verbose=1,
                             workers=1,
                             use_multiprocessing=False
                         )
-    print("Accuracy before attack is: ", performance_pre_attack[1]) 
+    print("Accuracy before attack is: ", performance_pre_attack[1])
 
-    ### Perform attack 
-    # Calculate grad w.r.t input for all test images 
+    ### Perform attack
+    # Calculate grad w.r.t input for all test images
     print("Calculating gradient w.r.t input..")
     calc_grads = create_gradient_function(model, 0, 0)
-    grads_X_test = [] 
+    grads_X_test = []
     temp_testdgen = test_dgen.flow(testX, testY, batch_size=50, shuffle=False)
     for batch_idx in tqdm(range(len(testX)//50)):
         x_batch, y_batch = next(temp_testdgen)
-        _, grads_batch = calc_grads([x_batch, y_batch]) 
+        _, grads_batch = calc_grads([x_batch, y_batch])
         grads_X_test.append(grads_batch)
-    grads_X_test = np.concatenate(grads_X_test, axis=0) 
+    grads_X_test = np.concatenate(grads_X_test, axis=0)
 
     # attacked = orig + eta*sign(grad)
-    assert grads_X_test.shape == testX.shape 
+    assert grads_X_test.shape == testX.shape
     attacked_testX = testX + eta*np.sign(grads_X_test)
 
-    # Performance after attack 
+    # Performance after attack
     performance_post_attack = model.evaluate_generator(
-                            generator=test_dgen.flow(attacked_testX, testY, batch_size=50),
+                            generator=test_dgen.flow(attacked_testX, testY, batch_size=50, shuffle=False),
                             steps=len(attacked_testX)/50,
                             verbose=1
                         )
     print("Accuracy after attack is: ", performance_post_attack[1])
-
-
